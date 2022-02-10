fd7ccec45dc037d9d3765c3bacab0fab9ea60df3
Ding Chengeng <582366665@163.com>
2022-02-10 17:57:48 +0800

    注册后端实现

--- meiduo_mall/meiduo_mall/apps/contents/urls.py
+++ meiduo_mall/meiduo_mall/apps/contents/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^register/$', views.RegisterView.as_view(), name='register'),
+
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,11 +1,1 @@
-from django.shortcuts import render
-from django.views import View
-
-# Create your views here.
-
-
-class RegisterView(View):
-
-    def get(self, request):
-
-        return render(request, 'register.html')
+import refrom django.shortcuts import render, redirectfrom django.views import Viewfrom django import httpfrom django.db import DatabaseErrorfrom django.urls import reversefrom users.models import User# Create your views here.class RegisterView(View):    def get(self, request):        return render(request, 'register.html')    def post(self, request):        username = request.POST.get('username')        password = request.POST.get('password')        password2 = request.POST.get('password2')        mobile = request.POST.get('mobile')        allow = request.POST.get('allow')        if not all([username, password, password2, mobile, allow]):            return http.HttpResponseForbidden('缺少必传参数')        if not re.match(r'^[0-9a-zA-Z_-]{5,20}$', username):            return http.HttpResponseForbidden('请输入5-20个字符的用户名')        if not re.match(r'^[0-9a-zA-Z]{8,20}$', password):            return http.HttpResponseForbidden('请输入8-20位的密码')        if password != password2:            return http.HttpResponseForbidden('两次输入的密码不一致')        if not re.match(r'^1[3-9]\d{9}$', mobile):            return http.HttpResponseForbidden('您输入的手机号格式不正确')        if allow != 'on':            return http.HttpResponseForbidden('请勾选用户协议')        try:            User.objects.create_user(username=username, password=password, mobile=mobile)        except DatabaseError:            return render(request, 'register.html', context={'register_errmsg': '注册失败'})        return redirect(reverse('contents:index'))
